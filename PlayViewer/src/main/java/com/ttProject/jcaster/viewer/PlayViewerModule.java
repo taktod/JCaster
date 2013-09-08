4c6548eff93a69f6acbdeb5f02b907ba236dcfeb
taktod <poepoemix@hotmail.com>
2013-09-08 10:43:17 +0900

    playViewerの作成途上だけど、とりあえずまとめてみた。 JCaster.zip回答してrun.batもしくはrun.shを実行すれば動作すると思う。 windowsXPのネットブックとiMac(MountainLion)で一応動作することは確認しといた。

--- PlayViewer/src/main/java/com/ttProject/jcaster/viewer/PlayViewerModule.java
+++ PlayViewer/src/main/java/com/ttProject/jcaster/viewer/PlayViewerModule.java
@@ -1,12 +1,14 @@
 package com.ttProject.jcaster.viewer;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 
@@ -28,7 +30,21 @@ public class PlayViewerModule implements IViewerModule, IOutputModule {
 	/** ロガー */
 	private Logger logger = Logger.getLogger(PlayViewerModule.class);
 	private FlvDecoder decoder = new FlvDecoder();
-	
+	private final JButton playButton;
+	private final JCheckBox videoCheckBox;
+	private final JCheckBox audioCheckBox;
+	private final JSlider volumeSlider;
+	private final JProgressBar vuMeterProgressBar;
+	/**
+	 * コンストラクタ
+	 */
+	public PlayViewerModule() {
+		playButton = new JButton("play");
+		videoCheckBox = new JCheckBox("video");
+		audioCheckBox = new JCheckBox("audio");
+		volumeSlider = new JSlider(JSlider.VERTICAL);
+		vuMeterProgressBar = new JProgressBar(JProgressBar.VERTICAL);
+	}
 	public void setup(JPanel panel) {
 		logger.info("転送中のデータを表示するviewをセットアップします。");
 		ISwingMainBase mainbase = BaseHandler.getISwingMainBase();
@@ -49,19 +65,23 @@ public class PlayViewerModule implements IViewerModule, IOutputModule {
 		panel.add(scrollPane, BorderLayout.CENTER);
 		// 右にコントロール部をあつめておく。
 		JPanel control = new JPanel();
-		panel.add(control, BorderLayout.LINE_END);
+		scrollPane = new JScrollPane(control, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		panel.add(scrollPane, BorderLayout.LINE_END);
 		GroupLayoutEx layout = new GroupLayoutEx(control);
 		control.setLayout(layout);
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
 
-		JSlider slider = new JSlider(JSlider.VERTICAL);
+		Dimension d = volumeSlider.getSize();
+		d.height = 200;
+		volumeSlider.setSize(d);
+		volumeSlider.setPreferredSize(d);
 		Object[][] components = {
-				{new JButton("▶")},
-				{new JCheckBox("video")},
-				{new JCheckBox("sound")},
+				{playButton},
+				{videoCheckBox},
+				{audioCheckBox},
 				{new JLabel("volume")},
-				{slider}
+				{new JComponent[]{volumeSlider, vuMeterProgressBar}}
 		};
 		layout.addComponents(components);
 		control.validate();
@@ -70,7 +90,6 @@ public class PlayViewerModule implements IViewerModule, IOutputModule {
 	@Override
 	public void onTimerEvent() {
 		// タイマーによる動作
-//		logger.info("timeきたよ");
 	}
 
 	@Override
--- readme.txt
+++ readme.txt
@@ -1,5 +1,8 @@
 JCaster
 
+内容:
+linuxやmacでxsplitみたいなのがないし、rtmpに対する理解が進んできたのでjavaで配信クライアント書いてみたい。
+
 作者:taktod
 
 コンタクト
@@ -8,33 +11,41 @@ blog: http://poepoemix.blogspot.jp/
 
 ライセンス：
 LGPL
- ConsoleViewer
- FlvSavePlugin
- JCaster
- JCaster.Plugin
- Mp4ReaderPlugin
- NoMixerPlugin
- RtmpPublishPlugin
+ ConsoleViewer:log4jの出力を表示する
+ FlvSavePlugin:flvのデータを保存する
+ JCaster:本体
+ JCaster.Plugin:プラグインのベース
+ Mp4ReaderPlugin:mp4のデータを読み取る
+ NoMixerPlugin:なにもmixしないmixer
+ RtmpPublishPlugin:rtmpでデータを配信する
 
 GPL
+ playViewer:再生中のデータ確認 xuggle利用 GPL(作成中)
 
 メモ：
 とりあえずLGPLで出します。
-今後xuggleをつかった変換処理も取り込む予定ですが、そのときには、対象プラグインはGPLになる予定。
+xuggleのライブラリを使うと基本GPLになるのでxuggleが絡んでいるプラグインはGPLになります。
 
 一応flvのコーデック変換や映像や音声を混ぜる動作はすでにmyLib.xuggleで実験済みだし。
 カメラの取り込み(vlcjやxuggleにやらせる？)やデスクトップキャプチャ(javaのrobotで可能)でできる予定。
 
 xsplitみたいに高機能になったら面白いですね。
-いまのところできるのは、mp4→flv変換。と任意のmp4をrtmpに配信すること。
+いまのところできるのは、mp4(h264 + aac)のデータを元にして
+mp4→flv変換。と任意のmp4をrtmpに配信すること。
 
 今後やること予定：
-・playViewer(再生中の動画視聴部) xuggle利用 GPL
-・MixerPlugin(コーデック変換) xuggle利用 GPL
-・FlvReaderPlugin(flvを流す動作) LGPL
-・RtmpReaderPlugin(他のサーバーに流れておるデータを流す動作) LGPL
-・multiReaderPlugin(いくつかのソースを組み合わせる動作) GPL
+・MixerPlugin:コーデック変換 xuggle利用 GPL
+・FlvReaderPlugin:flvのデータを読み取る LGPL
+・RtmpReaderPlugin:rtmpのデータを読み取る LGPL
+・DesktopReaderPlugin:デスクトップのデータを読み取る LGPL
+・CameraReaderPlugin:カメラのデータを読み取る ?
+・AudioReaderPlugin:音声データを読み取る LGPL?
+・MultiReaderPlugin:読み込みソースを混ぜたものを読み取る GPL
 というのをできるところからやっていきたいところ。
-とりあえずRtmpPublishPluginは早めに終わらせたいですね。
+すでにつくったプラグインについてもまだ突貫レベルなので、いろいろとバグがありそうだ。
+
+とりあえずは、playViewerが強制再生になっているので、これをやめたいところ。あと家のwinXPで動作させるとrtmp通信が追いつかなくなっていくので(thread使い過ぎか？)そこを改善したいところ。
+
+なにかありましたら、twitterあたりでコンタクトとってみてください。
 
 うまくいくといいけど・・・
