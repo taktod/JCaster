0d0bcd6deeea89c8fba606a258322e543f273a46
taktod <poepoemix@hotmail.com>
2013-09-14 21:25:15 +0900

    データの表示(音声再生も)つくってみていますが、どうも動作が芳しくないです。 うーん。

--- PlayViewer/src/main/java/com/ttProject/jcaster/player/decode/FlvDecoder.java
+++ PlayViewer/src/main/java/com/ttProject/jcaster/player/decode/FlvDecoder.java
@@ -1,267 +1,258 @@
 package com.ttProject.jcaster.player.decode;
 
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
 
 import org.apache.log4j.Logger;
 
 import com.ttProject.jcaster.player.swing.VideoComponent;
-import com.ttProject.media.flv.CodecType;
 import com.ttProject.media.flv.Tag;
 import com.ttProject.media.flv.tag.AudioTag;
 import com.ttProject.media.flv.tag.VideoTag;
-import com.ttProject.media.h264.ConfigData;
-import com.ttProject.media.h264.DataNalAnalyzer;
-import com.ttProject.media.h264.Frame;
-import com.ttProject.media.h264.frame.PictureParameterSet;
-import com.ttProject.media.h264.frame.SequenceParameterSet;
-import com.ttProject.media.h264.frame.SliceIDR;
-import com.ttProject.nio.channels.ByteReadChannel;
-import com.ttProject.nio.channels.IReadChannel;
-import com.xuggle.ferry.IBuffer;
-import com.xuggle.xuggler.ICodec;
+import com.ttProject.media.raw.VideoData;
+import com.ttProject.xuggle.flv.FlvPacketizer;
+import com.xuggle.xuggler.IAudioSamples;
 import com.xuggle.xuggler.IPacket;
 import com.xuggle.xuggler.IPixelFormat;
-import com.xuggle.xuggler.IRational;
 import com.xuggle.xuggler.IStreamCoder;
 import com.xuggle.xuggler.IVideoPicture;
 import com.xuggle.xuggler.IVideoResampler;
-import com.xuggle.xuggler.IStreamCoder.Direction;
 import com.xuggle.xuggler.video.ConverterFactory;
 import com.xuggle.xuggler.video.IConverter;
 
 /**
- * flvTagデータをデコードして、表示します。
- * とりあえず映像を表示するだけでもやっておきたいところ。
- * 音声の再生を実施するとたぶん、音声再生中にCPUが占領される現象があるので、そうしないようにしておくことにします。
- * 映像データの再生が安定しないのは、100ミリ秒ごとにデータの塊がとんでくるため、時間で制御をきちんとすれば、安定した再生になるはず。
+ * flvのデータをデコードして、再生する部分
  * @author taktod
+ * TODO ループスレッド処理ってまとめることができそうな気がする
  */
 public class FlvDecoder implements Runnable {
 	/** ロガー */
-	private Logger logger = Logger.getLogger(FlvDecoder.class);
-	// 再生時のボリューム値
-	private int volume;
+	private final Logger logger = Logger.getLogger(FlvDecoder.class);
+	/** ボリューム 0 - 100 */
+	private int volume = 100;
 	/** データ表示コンポーネント */
-	private VideoComponent component;
-	/** 処理スレッド */
-	private Thread decodeWorker = null;
-	// ここにデータをためていって、再生させる。
-	private LinkedBlockingQueue<Tag> dataQueue = new LinkedBlockingQueue<Tag>();
+	private final VideoComponent component;
+	/** 音声出力コンポーネント */
+	private SourceDataLine audioLine = null;
+	/** 音声出力フォーマット */
+	private AudioFormat audioFormat = null;
+	/** 処理スレッド(処理に時間がかかるので、別スレッドで実行) */
+	private final Thread decodeWorker;
+	/** 送信データを一時的にためるqueue */
+	private final LinkedBlockingQueue<Tag> dataQueue;
+//	private final LinkedBlockingDeque<Tag> dataQueue;
+	/** threadの動作フラグ */
 	private boolean workingFlg = true;
-	private IStreamCoder videoDecoder = null;
+	/** パケット復元用オブジェクト */
+	private final FlvPacketizer audioPacketizer;
+	private final FlvPacketizer videoPacketizer;
+	private long pictureFirstTimestamp = -1;
+	private AudioTag lastAudioTag = null;
+	private VideoTag lastVideoTag = null;
 	private IStreamCoder audioDecoder = null;
-	
-	// 処理用のパケットを作成しておく。
-	private IPacket packet = IPacket.make();
-	
-	// h264用の動作補助
-	private SequenceParameterSet sps;
-	private PictureParameterSet pps;
+	private IStreamCoder videoDecoder = null;
+	private long nextAcceptVideoTimestamp = -1;
+	// もっとくのは映像のみでOK
+//	private final List<VideoData> videoDataList = new ArrayList<VideoData>();
+	private final LinkedList<VideoData> videoDataQueue = new LinkedList<VideoData>();
 	/**
 	 * コンストラクタ
 	 */
 	public FlvDecoder() {
-		component = new VideoComponent();
+		// 映像出力先の設定
+		component =new VideoComponent();
+		// audio出力の設定
+		dataQueue = new LinkedBlockingQueue<Tag>();
+//		dataQueue = new LinkedBlockingDeque<Tag>();
+		audioPacketizer = new FlvPacketizer();
+		videoPacketizer = new FlvPacketizer();
 		decodeWorker = new Thread(this);
 		decodeWorker.setDaemon(true);
-		decodeWorker.setName("FlvDecoderThread");
+		decodeWorker.setName("PlayViewerWorker");
+		decodeWorker.setPriority(1);
 		decodeWorker.start();
 	}
+	/**
+	 * videoコンポーネント
+	 * @return
+	 */
 	public VideoComponent getComponent() {
 		return component;
 	}
 	/**
-	 * 処理すべきtagを設定する。
+	 * 処理すべきtagを追加する。
 	 * @param tag
 	 */
 	public void addTag(Tag tag) {
-		// とりあえずデコードしてvideoTagだった場合に映像を出すことにします。
 		dataQueue.add(tag);
 	}
+	/**
+	 * 処理動作
+	 */
 	@Override
 	public void run() {
 		try {
 			while(workingFlg) {
-				if(dataQueue.size() == 0) {
-					// threadのデータがなくなってきたら他のthreadの動作を促す。
-					Thread.yield();
-				}
-				Tag tag = dataQueue.take();
-				if(tag == null) {
-					break;
-				}
-				if(tag instanceof VideoTag) {
-					// 映像データの場合
-					VideoTag vTag = (VideoTag) tag;
-					if(vTag.getCodec() != CodecType.AVC) {
-						// h264以外うまく動作できないので、とりあえず処理しないようにしておく。
-						continue;
+				List<Tag> tagList = new ArrayList<Tag>();
+				tagList.add(dataQueue.take());
+				while(true) {
+					Tag tag = dataQueue.poll();
+					if(tag == null) {
+						break;
 					}
-					// デコーダーについて調査する。
-					checkDecoder(tag);
-					if(vTag.getCodec() == CodecType.AVC) {
-						if(vTag.isMediaSequenceHeader()) {
-							// mshの場合はspsとppsを取得する。
-							ConfigData configData = new ConfigData();
-							IReadChannel rawData = new ByteReadChannel(vTag.getRawData());
-							rawData.position(3);
-							List<Frame> nals = configData.getNals(rawData);
-							for(Frame nal : nals) {
-								if(nal instanceof SequenceParameterSet) {
-									sps = (SequenceParameterSet) nal;
-								}
-								else if(nal instanceof PictureParameterSet) {
-									pps = (PictureParameterSet) nal;
-								}
-							}
-							continue;
-						}
-						// avcの場合
-						ByteBuffer rawData = vTag.getRawData();
-						rawData.position(7);
-						int size = rawData.remaining();
-						IBuffer bufData = null;
-						if(vTag.isKeyFrame()) {
-							DataNalAnalyzer dataAnalyzer = new DataNalAnalyzer();
-							rawData.position(3);
-							IReadChannel rawDataChannel = new ByteReadChannel(rawData);
-							Frame h264Frame = null;
-							while((h264Frame = dataAnalyzer.analyze(rawDataChannel)) != null) {
-								if(h264Frame instanceof SliceIDR) {
-									break;
-								}
-							}
-							packet.setKeyPacket(true);
-							ByteBuffer spsData = sps.getData();
-							ByteBuffer ppsData = pps.getData();
-							ByteBuffer sliceIDRData = h264Frame.getData();
-							ByteBuffer buffer = ByteBuffer.allocate(sliceIDRData.remaining() + 4 + spsData.remaining() + 4 + ppsData.remaining() + 4);
-							buffer.putInt(1);
-							buffer.put(spsData);
-							buffer.putInt(1);
-							buffer.put(ppsData);
-							buffer.putInt(1);
-							buffer.put(sliceIDRData);
-							buffer.flip();
-							size = buffer.remaining();
-							bufData = IBuffer.make(null, buffer.array(), 0, size);
-						}
-						else {
-							packet.setKeyPacket(false);
-							ByteBuffer buffer = ByteBuffer.allocate(rawData.remaining() + 4);
-							buffer.putInt(1);
-							buffer.put(rawData.array(), 7, size);
-							buffer.flip();
-							size = buffer.remaining();
-							bufData = IBuffer.make(null, buffer.array(), 0, size);
-						}
-						packet.setData(bufData);
-						packet.setFlags(1);
-						packet.setDts(vTag.getTimestamp());
-						packet.setPts(vTag.getTimestamp());
-						packet.setTimeBase(IRational.make(1, 1000));
-						packet.setComplete(true, size);
-
-						// データができたので、pictureに変換します。
-						IVideoPicture picture = IVideoPicture.make(videoDecoder.getPixelType(), videoDecoder.getWidth(), videoDecoder.getHeight());
-						int offset = 0;
-						while(offset < packet.getSize()) {
-							int bytesDecoded = videoDecoder.decodeVideo(picture, packet, offset);
-							if(bytesDecoded < 0) {
-								throw new Exception("デコードに失敗しました。");
-							}
-							offset += bytesDecoded;
-							if(picture.isComplete()) {
-								IVideoPicture newPic = picture;
-								if(picture.getPixelType() != IPixelFormat.Type.BGR24) {
-									IVideoResampler resampler = IVideoResampler.make(videoDecoder.getWidth(), videoDecoder.getHeight(), IPixelFormat.Type.BGR24, videoDecoder.getWidth(), videoDecoder.getHeight(), videoDecoder.getPixelType());
-									newPic = IVideoPicture.make(resampler.getOutputPixelFormat(), picture.getWidth(), picture.getHeight());
-									if(resampler.resample(newPic, picture) < 0) {
-										throw new Exception("リサンプル失敗しました。");
-									}
-								}
-								IConverter converter = ConverterFactory.createConverter("XUGGLER-BGR-24", newPic);
-								component.setImage(converter.toImage(newPic));
-							}
-						}
-					}
-					else {
-						// その他のデータの場合
-						ByteBuffer rawData = vTag.getRawData();
-						IBuffer bufData = null;
-						int size = rawData.remaining();
-						packet.setKeyPacket(vTag.isKeyFrame());
-						bufData = IBuffer.make(null, rawData.array(), 0, size);
-						packet.setData(bufData);
-						packet.setFlags(1);
-						packet.setDts(vTag.getTimestamp());
-						packet.setPts(vTag.getTimestamp());
-						packet.setTimeBase(IRational.make(1, 1000));
-						packet.setComplete(true, size);
-						// データができたので、pictureに変換します。
-						IVideoPicture picture = IVideoPicture.make(videoDecoder.getPixelType(), videoDecoder.getWidth(), videoDecoder.getHeight());
-						int offset = 0;
-						while(offset < packet.getSize()) {
-							int bytesDecoded = videoDecoder.decodeVideo(picture, packet, offset);
-							if(bytesDecoded < 0) {
-								throw new Exception("デコードに失敗しました。");
-							}
-							offset += bytesDecoded;
-							if(picture.isComplete()) {
-								IVideoPicture newPic = picture;
-								if(picture.getPixelType() != IPixelFormat.Type.BGR24) {
-									IVideoResampler resampler = IVideoResampler.make(videoDecoder.getWidth(), videoDecoder.getHeight(), IPixelFormat.Type.BGR24, videoDecoder.getWidth(), videoDecoder.getHeight(), videoDecoder.getPixelType());
-									newPic = IVideoPicture.make(resampler.getOutputPixelFormat(), picture.getWidth(), picture.getHeight());
-									if(resampler.resample(newPic, picture) < 0) {
-										throw new Exception("リサンプル失敗しました。");
-									}
-								}
-								IConverter converter = ConverterFactory.createConverter("XUGGLER-BGR-24", newPic);
-								component.setImage(converter.toImage(newPic));
-							}
-						}
+					tagList.add(tag);
+				}
+				for(Tag tag : tagList) {
+					if(tag instanceof AudioTag) {
+						processAudio((AudioTag)tag);
 					}
 				}
-				else if(tag instanceof AudioTag) {
-					
+				for(Tag tag : tagList) {
+					if(tag instanceof VideoTag) {
+						processVideo((VideoTag)tag);
+					}
 				}
+/*				if(tag != null) {
+					if(tag instanceof VideoTag) {
+						processVideo((VideoTag)tag);
+					}
+					else if(tag instanceof AudioTag) {
+						processAudio((AudioTag)tag);
+					}
+				}*/
+				updatePicture();
 			}
 		}
 		catch (Exception e) {
-			logger.error("デコード処理で失敗しました。", e);
+			logger.error("処理に失敗しました。", e);
 		}
 	}
-	private void checkDecoder(Tag tag) throws Exception {
-		if(tag instanceof AudioTag) {
-			// オーディオはとりあえず放置
+	/**
+	 * 映像を更新する。
+	 * 音声はaudioLineが自動的に処理するので、そちらにまかせる。
+	 */
+	private void updatePicture() {
+		if(audioLine == null || audioFormat == null) {
+			return;
 		}
-		else if(tag instanceof VideoTag) {
-			VideoTag vTag = (VideoTag) tag;
-			// デコーダーが未設定もしくはコーデックが一致していない場合
-			if(videoDecoder == null
-					|| (vTag.getCodec() == CodecType.H263 && videoDecoder.getCodecID() != ICodec.ID.CODEC_ID_FLV1)
-					|| (vTag.getCodec() == CodecType.ON2VP6 && videoDecoder.getCodecID() != ICodec.ID.CODEC_ID_VP6F)
-					|| (vTag.getCodec() == CodecType.ON2VP6_ALPHA && videoDecoder.getCodecID() != ICodec.ID.CODEC_ID_VP6A)
-					|| (vTag.getCodec() == CodecType.AVC && videoDecoder.getCodecID() != ICodec.ID.CODEC_ID_H264)) {
-				if(vTag.getCodec() == CodecType.AVC) {
-					videoDecoder = IStreamCoder.make(Direction.DECODING, ICodec.ID.CODEC_ID_H264);
+		long timestamp = (long)(audioLine.getLongFramePosition() / audioFormat.getSampleRate() * 1000);
+		VideoData videoData = null;
+		while(videoDataQueue.size() != 0) {
+			VideoData vData = videoDataQueue.getFirst();
+			if(vData.getTimestamp() > timestamp) {
+				break;
+			}
+			videoData = videoDataQueue.removeFirst();
+		}
+		if(videoData != null) {
+			component.setImage(videoData.getImage());
+		}
+	}
+	/**
+	 * 音声データの処理
+	 * @throws Exception
+	 */
+	private void processAudio(AudioTag tag) throws Exception {
+		IPacket packet = audioPacketizer.getPacket(tag);
+		if(packet == null) {
+			return;
+		}
+		if(lastAudioTag == null || 
+				lastAudioTag.getCodec() != tag.getCodec() || 
+				lastAudioTag.getChannels() != tag.getChannels() ||
+				lastAudioTag.getSampleRate() != tag.getSampleRate()) {
+			audioDecoder = audioPacketizer.createAudioDecoder();
+			if(audioLine != null) {
+				audioLine.close();
+				audioLine = null;
+			}
+			// デコーダーが更新されているのでaudioLineも更新する。
+			audioFormat = new AudioFormat(audioDecoder.getSampleRate(),
+					(int)IAudioSamples.findSampleBitDepth(audioDecoder.getSampleFormat()),
+					audioDecoder.getChannels(), true, false);
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+			audioLine = (SourceDataLine)AudioSystem.getLine(info);
+			audioLine.open(audioFormat);
+			audioLine.start();
+		}
+		lastAudioTag = tag;
+		int offset = 0;
+		IAudioSamples samples = IAudioSamples.make(1024, audioDecoder.getChannels());
+		while(offset < packet.getSize()) {
+			int bytesDecoded = audioDecoder.decodeAudio(samples, packet, offset);
+			if(bytesDecoded < 0) {
+				throw new Exception("デコード中にエラーが発生");
+			}
+			offset += bytesDecoded;
+			if(samples.isComplete()) {
+				byte[] rawBytes = samples.getData().getByteArray(0, samples.getSize());
+				if(audioLine != null) {
+					audioLine.write(rawBytes, 0, samples.getSize());
 				}
-				else if(vTag.getCodec() == CodecType.H263) {
-					videoDecoder = IStreamCoder.make(Direction.DECODING, ICodec.ID.CODEC_ID_FLV1);
+			}
+		}
+	}
+	/**
+	 * 映像データの処理
+	 * @throws Exception
+	 */
+	private void processVideo(VideoTag tag) throws Exception {
+		IPacket packet = videoPacketizer.getPacket(tag);
+		if(packet == null) {
+			return;
+		}
+		if(lastVideoTag == null || lastVideoTag.getCodec() != tag.getCodec()) {
+			// コーデックが違う場合は取り直す必要あり
+			videoDecoder = videoPacketizer.createVideoDecoder();
+		}
+		lastVideoTag = tag;
+		int offset = 0;
+		IVideoPicture picture = IVideoPicture.make(videoDecoder.getPixelType(), videoDecoder.getWidth(), videoDecoder.getHeight());
+		while(offset < packet.getSize()) {
+			int bytesDecoded = videoDecoder.decodeVideo(picture, packet, offset);
+			if(bytesDecoded < 0) {
+				throw new Exception("映像のデコード中に問題が発生しました。");
+			}
+			offset += bytesDecoded;
+			if(picture.isComplete()) {
+				// 変換はリアルタイムに実行可能っぽい。
+				// よってあまりにも密度が濃い場合はフレームをスキップするようにしてやればとりあえずなんとかできそう。
+				// 30fpsくらいは表示してやりたいところだが・・・どうするかな・・・
+				if(tag.getTimestamp() < nextAcceptVideoTimestamp) {
+					// 前のvideoデータの処理に時間がかかりすぎている場合は次のデータをスキップする。
+					System.out.println("時間かかりすぎを検知したので、スキップします。");
+					System.out.println(tag);
+					return;
 				}
-				else if(vTag.getCodec() == CodecType.ON2VP6) {
-					videoDecoder = IStreamCoder.make(Direction.DECODING, ICodec.ID.CODEC_ID_VP6F);
+				// 最終のデータ変換に時間がかかった場合、次のデータ変換はパスするようにすればいい感じか？
+				long startTime = System.currentTimeMillis();
+				IVideoPicture newPic = picture;
+				if(picture.getPixelType() != IPixelFormat.Type.BGR24) {
+					IVideoResampler resampler = IVideoResampler.make(videoDecoder.getWidth(), videoDecoder.getHeight(), IPixelFormat.Type.BGR24, picture.getWidth(), picture.getHeight(), picture.getPixelType());
+					newPic = IVideoPicture.make(resampler.getOutputPixelFormat(), resampler.getOutputWidth(), resampler.getOutputHeight());
+					if(resampler.resample(newPic, picture) < 0) {
+						throw new Exception("映像リサンプル失敗");
+					}
 				}
-				else if(vTag.getCodec() == CodecType.ON2VP6_ALPHA) {
-					videoDecoder = IStreamCoder.make(Direction.DECODING, ICodec.ID.CODEC_ID_VP6A);
-				}
-				else {
-					throw new RuntimeException("処理不能なコーデックを検知しました。" + vTag.getCodec());
-				}
-				if(videoDecoder.open(null, null) < 0) {
-					throw new Exception("デコーダーが開けませんでした。");
+//				if(pictureFirstTimestamp == -1) {
+//					pictureFirstTimestamp = picture.getTimeStamp();
+//				}
+				IConverter converter = ConverterFactory.createConverter("XUGGLER-BGR-24", newPic);
+//				videoDataList.add(new VideoData(converter.toImage(newPic), (long)(newPic.getTimeStamp() * newPic.getTimeBase().getDouble() * 1000)));
+//				videoDataQueue.add(new VideoData(converter.toImage(newPic), (long)((picture.getTimeStamp() - pictureFirstTimestamp) / 10000)));
+				videoDataQueue.add(new VideoData(converter.toImage(newPic), tag.getTimestamp()));
+				nextAcceptVideoTimestamp = System.currentTimeMillis() - startTime + tag.getTimestamp() + 5;
+				if(nextAcceptVideoTimestamp < 100 + tag.getTimestamp()) {
+					nextAcceptVideoTimestamp = 100 + tag.getTimestamp();
 				}
 			}
 		}
