8fd6a4545c4f077595dff49c726f5e1cea61040b
taktod_win8 <poepoemix@hotmail.com>
2013-10-06 19:13:57 +0900

    実際に利用するプログラム側をつかって動作テストできるように調整しました。

--- PlayViewer/src/test/java/com/ttProject/jcaster/player/threadtest/PlayerTest.java
+++ PlayViewer/src/test/java/com/ttProject/jcaster/player/threadtest/PlayerTest.java
@@ -3,8 +3,6 @@ package com.ttProject.jcaster.player.threadtest;
 import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import org.junit.Test;
 
@@ -14,12 +12,8 @@ import com.ttProject.media.flv.FlvHeader;
 import com.ttProject.media.flv.ITagAnalyzer;
 import com.ttProject.media.flv.Tag;
 import com.ttProject.media.flv.TagAnalyzer;
-import com.ttProject.media.flv.tag.AudioTag;
-import com.ttProject.media.flv.tag.VideoTag;
 import com.ttProject.nio.channels.FileReadChannel;
 import com.ttProject.nio.channels.IFileReadChannel;
-import com.ttProject.xuggle.flv.FlvPacketizer;
-import com.xuggle.xuggler.IPacket;
 
 /**
  * 実際に動画の再生を実施する動作テスト
@@ -44,7 +38,6 @@ public class PlayerTest {
 				workingFlg = false;
 			};
 		});
-//		File fileTarget = new File("C:\\Users\\taktod\\Downloads\\mario.flv");
 		// flvデータを読み込んで動作させる。
 		/*
 		 * 基本的にFlvDecoderにデータを流す
@@ -57,67 +50,9 @@ public class PlayerTest {
 		 * とりあえずいきなり本家でつくるとややこしいので、それぞれのクラスをつくってやっていこうと思う
 		 */
 		ITagAnalyzer analyzer = new TagAnalyzer();
-//		IFileReadChannel ch = FileReadChannel.openFileReadChannel(fileTarget.getAbsolutePath());
 		IFileReadChannel ch = FileReadChannel.openFileReadChannel("http://49.212.39.17/mario.flv");
 		FlvHeader header = new FlvHeader();
 		header.analyze(ch);
-/*
-		// データの受け渡し用
-		final LinkedBlockingQueue<Tag> videoQueue = new LinkedBlockingQueue<Tag>();
-		final LinkedBlockingQueue<Tag> audioQueue = new LinkedBlockingQueue<Tag>();
-		// ここで一度packetをつくっておかないと各threadで詰まることがあるっぽい。
-		// Threadの準備
-		Thread videoThread = new Thread(new Runnable() {
-			IPacket packet = IPacket.make();
-			FlvPacketizer packetizer = new FlvPacketizer();
-			@Override
-			public void run() {
-				try {
-					while(true) {
-						Tag tag = videoQueue.take();
-						System.out.println(tag);
-						IPacket packet = packetizer.getPacket(tag, this.packet);
-						System.out.println("packet化おわり");
-					}
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		videoThread.start();
-		Thread audioThread = new Thread(new Runnable() {
-			IPacket packet = IPacket.make();
-			FlvPacketizer packetizer = new FlvPacketizer();
-			@Override
-			public void run() {
-				try {
-					while(true) {
-						Tag tag = audioQueue.take();
-						System.out.println(tag);
-						IPacket packet = packetizer.getPacket(tag, this.packet);
-						System.out.println("packet化おわり");
-					}
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		audioThread.start();
-		Tag tag = null;
-		while((tag = analyzer.analyze(ch)) != null) {
-			if(tag instanceof AudioTag) {
-				audioQueue.add(tag);
-			}
-			else if(tag instanceof VideoTag) {
-				videoQueue.add(tag);
-			}
-//			System.out.println(tag);
-		}
-		videoThread.interrupt();
-		audioThread.interrupt();
-		*/
 		FlvDecoder flvDecoder = new FlvDecoder();
 		frame.add(flvDecoder.getComponent());
 		Tag tag = null;
