9f3a767192909ad16ba764db58bb6f3065b665a6
taktod_win8 <poepoemix@hotmail.com>
2013-10-10 06:22:46 +0900

    データtakeテスト

---
+++ PlayViewer/src/test/java/com/ttProject/jcaster/player/threadtest/TakeTest.java
@@ -0,0 +1,35 @@
+package com.ttProject.jcaster.player.threadtest;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author taktod
+ *
+ */
+public class TakeTest {
+	@Test
+	public void test() throws Exception {
+		System.out.println("test start");
+		final LinkedBlockingQueue<String> data = new LinkedBlockingQueue<String>();
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					System.out.println(data.take());
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		t.start();
+		System.out.println("try clear");
+		data.clear();
+		t.interrupt();
+		t.join();
+		System.out.println("test end");
+	}
+}
